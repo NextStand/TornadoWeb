bf6b1179a89ac5236a24d2fda25dd8250ccae525
NextStand <18215676683@163.com>
2018-09-06 22:03:43 +0800

    请求处理handler

--- application/handlers/MainHandler.py
+++ application/handlers/MainHandler.py
@@ -4,11 +4,13 @@ import json
 
 from application.base.BaseHandler import BaseHandler
 from application.business.BusinessTest import BusinessTest
+from utils.session import Session
 
 
 class IndexHandler(BaseHandler):
     @tornado.gen.coroutine
     def get(self):
         ret = yield BusinessTest.test()
-        ret={'data':str(ret)}
+        ret = {'data': str(ret)}
+        session = Session(self)
         self.write(ret)
