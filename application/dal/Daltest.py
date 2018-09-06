4b012f7d6fb116bba24fd53eea57dbc959650d81
NextStand <18215676683@163.com>
2018-09-06 22:03:20 +0800

    数据库数据操作句柄

--- application/dal/Daltest.py
+++ application/dal/Daltest.py
@@ -4,7 +4,7 @@ from application.db.MySQLHelper import MySQLHelper
 
 class __Daltest(MySQLHelper):
     def test(self):
-        return self.executemany('insert into ih_area_info(ai_name) values(%(name)s)',args=[{'name':'成华区'},{'name':'武侯区'}])
+        return self.fetchall('select * from ih_area_info where ai_area_id >= 3870;')
 
 
 Daltest = __Daltest()
