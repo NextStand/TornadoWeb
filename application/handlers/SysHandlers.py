84df9c3e87e528beda39dfee3de8da63fc2b66c7
NextStand <18215676683@163.com>
2018-09-07 09:51:38 +0800

    内置handler

---
+++ application/handlers/SysHandlers.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+__author__ = 'BLUE'
+__time__ = '2018-09-07T01:44:55.741Z'
+
+""" 
+This handlers module is some handlers built in the system. 
+Generally do not modify unless you need it very much.
+"""
