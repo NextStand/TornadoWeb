7ad882cb54ad3f8af3b7e48e249bef82f0890a5f
NextStand <18215676683@163.com>
2018-09-10 10:35:08 +0800

    完善用户身份验证

--- application/base/BaseHandler.py
+++ application/base/BaseHandler.py
@@ -4,47 +4,49 @@ __author__ = 'BLUE'
 import json
 
 from tornado.web import RequestHandler, StaticFileHandler
-# from utils.session import Session
+from utils.session import Session
 
 
 class BaseHandler(RequestHandler):
-    """自定义基类"""
-    @property
-    def db(self):
-        """作为RequestHandler对象的db属性"""
-        return self.application.db
-
+    """
+    自定义请求句柄基类
+    """
     @property
     def redis(self):
-        """作为RequestHandler对象的redis属性"""
+        """
+        作为RequestHandler对象的redis属性
+        """
         return self.application.redis
 
     def prepare(self):
-        # 设置token
         self.xsrf_token
-        # 预解析json数据
         if self.request.headers.get("Content-Type", "").startswith("application/json"):
             self.json_args = json.loads(self.request.body)
         else:
             self.json_args = {}
 
     def set_default_headers(self):
-        """设置默认json格式"""
+        """
+        设置默认json格式
+        """
         # self.set_header("Content-Type", "application/json; charset=UTF-8")
         pass
 
     def get_current_user(self):
-        """判断用户是否登录"""
-        # self.session = Session(self)
-        # return self.session.data
-        pass
+        """
+        @tornado.web.authenticated装饰器调用，用于用户验证
+        """
+        self.session = Session(self)
+        return self.session.data
 
     def on_finish(self):
         pass
 
 
 class StaticFileBaseHandler(StaticFileHandler):
-    """自定义静态文件处理类, 在用户获取html页面的时候设置_xsrf的cookie"""
+    """
+    自定义静态文件处理类, 在用户获取html页面的时候设置_xsrf的cookie
+    """
 
     def __init__(self, *args, **kwargs):
         super(StaticFileBaseHandler, self).__init__(*args, **kwargs)
