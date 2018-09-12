2bb9fa43324ffd56ed8deff88696f09a17681da2
NextStand <18215676683@163.com>
2018-09-12 10:17:46 +0800

    dal不再继承db，采用静态方法

--- application/dal/Daltest.py
+++ application/dal/Daltest.py
@@ -1,10 +1,8 @@
 # coding:utf-8
-from application.db.MySQLHelper import MySQLHelper
+from application.db.MySQLHelper import db
 
 
-class __Daltest(MySQLHelper):
-    def test(self):
-        return self.fetchall('select * from ih_area_info where ai_area_id >= 3870;')
-
-
-Daltest = __Daltest()
+class Daltest():
+    @staticmethod
+    def test():
+        return db.callproc('ins_area_proc')
