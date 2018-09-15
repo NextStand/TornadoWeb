19dba036a0ea206aee47038a03c415dab9b7c207
NextStand <18215676683@163.com>
2018-09-15 22:42:00 +0800

    公共工具

---
+++ utils/comm_tools.py
@@ -0,0 +1,14 @@
+import json
+from datetime import date, datetime
+
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.strftime('%Y-%m-%d %H:%M:%S')
+        if isinstance(obj, datetime):
+            return obj.strftime('%Y-%m-%d %H:%M:%S')
+        elif isinstance(obj, date):
+            return obj.strftime('%Y-%m-%d')
+        else:
+            return json.JSONEncoder.default(self, obj)
