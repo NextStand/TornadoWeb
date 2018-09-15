1fab040cb37cea18f9f8f96973501fd10b4ea41c
NextStand <18215676683@163.com>
2018-09-15 20:19:32 +0800

    更改session逻辑，数据有效期考虑网络超时时间

--- utils/session.py
+++ utils/session.py
@@ -5,7 +5,7 @@ import uuid
 import json
 import logging
 
-from constants import SESSION_EXPIRES_SECONDS
+from constants import SESSION_EXPIRES_SECONDS, REQUEST_TIMEOUT
 
 
 class Session(object):
@@ -18,17 +18,15 @@ class Session(object):
         :param request_handler:tornado.web.RequestHandler 请求句柄
         """
         self._request_handler = request_handler
-        self.session_id = request_handler.get_secure_cookie('session_id')
+        self.session_id = request_handler.get_secure_cookie('_BSSID')
         if not self.session_id:
             self.session_id = uuid.uuid4().hex
             self.data = {}
-            request_handler.set_secure_cookie(
-                "session_id", self.session_id, expires_days=SESSION_EXPIRES_SECONDS/(3600*24))
         else:
             json_data = None
             try:
                 json_data = request_handler.redis.get(
-                    "sess_%s" % self.session_id)
+                    "ssid_%s" % self.session_id)
             except Exception as e:
                 logging.error(e)
             if not json_data:
@@ -42,9 +40,10 @@ class Session(object):
         """
         json_data = json.dumps(self.data)
         try:
-            print(self._request_handler.redis)
+            request_handler.set_secure_cookie(
+                "_BSSID", self.session_id, expires_days=SESSION_EXPIRES_SECONDS/(3600*24))
             self._request_handler.redis.setex(
-                'sess_%s' % self.session_id, SESSION_EXPIRES_SECONDS, json_data)
+                'ssid_%s' % self.session_id, SESSION_EXPIRES_SECONDS + REQUEST_TIMEOUT, json_data)
         except Exception as e:
             logging.error(e)
             raise e
@@ -54,8 +53,8 @@ class Session(object):
         清空session
         """
         try:
-            self._request_handler.redis.delete('sess_%s' % self.session_id)
+            self._request_handler.redis.delete('ssid_%s' % self.session_id)
             self.data = {}
         except Exception as e:
             logging.error(e)
-        self._request_handler.clear_cookie("session_id")
+        self._request_handler.clear_cookie("_BSSID")
