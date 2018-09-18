132a430b22fd434ab64fb7043a9c1ef8a5d1addc
NextStand <18215676683@163.com>
2018-09-18 18:04:07 +0800

    啊哦，没错，还是测试

--- application/handlers/MainHandler.py
+++ application/handlers/MainHandler.py
@@ -2,15 +2,29 @@
 import tornado
 import json
 
+from tornado import gen
 from application.base.BaseHandler import BaseHandler
 from application.business.BusinessTest import BusinessTest
 from utils.session import Session
+from utils.SendSMS import cpp
+from libs.captcha.captcha import captcha
 
 
 class IndexHandler(BaseHandler):
-    @tornado.gen.coroutine
+    @gen.coroutine
     def get(self):
         ret = yield BusinessTest.test()
-        ret = {'data': str(ret)}
-        session = Session(self)
-        self.write(ret)
+        if ret:
+            self.write(str(ret))
+        else:
+            self.send_error(500, msg='数据库出错了')
+
+    @gen.coroutine
+    def post(self):
+        ret = yield BusinessTest.test()
+        self.write(str(ret))
+
+    def delete(self):
+        print self.request.arguments
+        a = self.get_argument('a', 'aaa')
+        self.write(a)
