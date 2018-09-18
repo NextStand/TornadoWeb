b1dba152bded09f4e5cf5b43671b77318d8be214
NextStand <18215676683@163.com>
2018-09-18 17:59:45 +0800

    添加静态文件处理路径

--- urls_admin.py
+++ urls_admin.py
@@ -1,5 +1,10 @@
 # coding:utf-8
+import os
+
 from application.base import BaseHandler
+from application.handlers import SysHandlers
 urls_admin = [
+    (r"/upload/(.*)", BaseHandler.StaticFileBaseHandler,
+     dict(path=os.path.join(os.path.dirname(__file__), "upload"))),
     (r".*", BaseHandler.BaseHandler)
 ]
